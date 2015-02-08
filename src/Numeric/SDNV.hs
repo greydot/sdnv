52174d742b7b076b44711cf2d78ec78c2e563763
Lana Black <lanablack@eml.cc>
2015-02-08 15:53:18 +0000

    haddock

--- src/Numeric/SDNV.hs
+++ src/Numeric/SDNV.hs
@@ -1,6 +1,6 @@
 {-# Language MultiWayIf #-}
 {-# Language ScopedTypeVariables #-}
-module Numeric.SDNV (SDNV
+module Numeric.SDNV (SDNV()
                     ,encodeSDNV
                     ,decodeSDNV
                     ,safeDecodeSDNV
@@ -28,6 +28,9 @@ instance Binary (SDNV a) where
                           in get >>= g
             | otherwise -> fail "Invalid SDNV"
 
+-- |Encode a value into SDNV.
+-- Please note that encoding of variable length negative values
+-- such as of type Integer results in sign lost.
 encodeSDNV :: (Bits a, Integral a) => a -> SDNV a
 encodeSDNV = SDNV . pack . encode []
     where
@@ -38,17 +41,26 @@ encodeSDNV = SDNV . pack . encode []
                           nv = n `shiftR` 7
                       in encode (v:vs) nv
 
+-- |Decode SDNV into an integral value.
+-- This function doesn't check SDNV integrity and size,
+-- and in certain cases may produce incorrect results.
+-- See 'safeDecodeSDNV'.
 decodeSDNV :: (Bits a, Integral a) => SDNV a -> a
 decodeSDNV (SDNV bs) = foldl decode 0 . unpack $ bs
     where
         decode r v = let dv = fromIntegral (v .&. 0x7F)
                      in (r `shiftL` 7) .|. dv
 
+-- |Decode SDNV into an intergral value.
+-- This function uses 'isValidSDNV' to detect incorrectly encoded SDNV,
+-- and to check for overflow in case of finite size types.
 safeDecodeSDNV :: (Bits a, Integral a) => SDNV a -> Maybe a
 safeDecodeSDNV s
     | isValidSDNV s = return $ decodeSDNV s
     | otherwise = Nothing
 
+-- |Check SDNV correctness.
+-- Returns True if SDNV is correctly encoded and decoding will not result in an overflow for finite datatypes.
 isValidSDNV :: forall a. (Bits a, Integral a) => SDNV a -> Bool
 isValidSDNV (SDNV bs) = let s = fromShort bs
                         in validateData s && validateSize (B.head s) (B.length s)
